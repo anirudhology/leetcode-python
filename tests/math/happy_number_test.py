67fdea9336537401c8ed4653897ff00d0b3de53e
anirudhology <anirudhology@gmail.com>
2024-08-27 06:30:40 +0530

    Add problem 202 - Happy Number

--- tests/math/happy_number_test.py
+++ tests/math/happy_number_test.py
@@ -21,5 +21,6 @@ class TestHappyNumber(unittest.TestCase):
         # Test number with multiple digits that is not happy
         self.assertFalse(self.happy_number.isHappy(21))
 
+
 if __name__ == '__main__':
     unittest.main()
